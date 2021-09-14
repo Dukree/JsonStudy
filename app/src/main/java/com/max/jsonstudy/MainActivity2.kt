aa011937e812618fa7fa6043d09750b99673870d
Dukree <under9090@gmail.com>
2021-09-14 17:59:20 +0300

    Third commit

--- app/src/main/java/com/max/jsonstudy/MainActivity2.kt
+++ app/src/main/java/com/max/jsonstudy/MainActivity2.kt
@@ -2,10 +2,30 @@ package com.max.jsonstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        read_json()
+    }
+    fun read_json(){
+        val jsntxt : TextView = findViewById(R.id.editTextTextPersonName)
+        var json: String? = null
+    try{
+        val inputStream:InputStream = assets.open("answers.json")
+        json = inputStream.bufferedReader().use { it.readText() }
+        jsntxt.text = json
+    }
+    catch (e:IOException)
+    {
+
+    }
     }
 }
